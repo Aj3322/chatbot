cef3bebbab01bc94f6fac92e605514060c873a8a
Niharika_Attri <niharikaattri2005@gmail.com>
2024-05-09 22:12:20 +0530

    .

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-while True:
-    user_input = input("What problems are you facing? (type 'exit' to quit): ")
-    if user_input.lower() == 'exit':
-        break
-    response_text = get_gemini_response(user_input+" keep the answer short and to the point. Only suggest the preventions")
-    display(to_markdown(response_text + '\n\nIs there anything else I can help you with?'))
-
